eaf89895175e6282d52d5e36d982e14c8bcd5b86
Taopik Hidayat <73512797+TaopikHidayat08@users.noreply.github.com>
2021-03-22 18:54:07 +0700

    Create Biodata_singkat.pas

---
+++ Tugas1/Biodata_singkat.pas
@@ -0,0 +1,18 @@
+{ Nama : Taopik Hidayat }
+{ Npm  : 20.14.1.0048   }
+{ Nama program : Biodata.pas }
+
+program biodatasingkat;
+uses crt;
+{Proses}
+begin
+writeln(' Biodata Singkat ');
+writeln(' Nama   : Taopik Hidayat ');
+writeln(' NPM    : 20.14.1.0048 ');
+writeln(' Alamat : Blok Ciburuy, Desa.Sukaraja Kulon, Kec.Jatiwangi, Kab.Majalengka ');
+writeln(' TTL    : Majalengka, 04 Desember 1999 ');
+writeln(' No. Hp : 0895357303491 ');
+writeln(' Motto  : Yakin adalah kunci jawaban dari segala permasalan. Dengan bermodal ');
+writeln('          yakin merupakan obat mujarab penumbuh semangat hidup. ');
+readln;
+end.
