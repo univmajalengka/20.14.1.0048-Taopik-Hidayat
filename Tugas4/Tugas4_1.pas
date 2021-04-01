b1f66bfdfb973b1364aa9794c8d3b2a907fd7f83
Taopik Hidayat <73512797+TaopikHidayat08@users.noreply.github.com>
2021-04-02 06:06:30 +0700

    Create Tugas4_1.pas

---
+++ Tugas4/Tugas4_1.pas
@@ -0,0 +1,24 @@
+{NAMA         : Taopik Hidayat                 }
+{NPM          : 20.14.1.0048                           }
+{NAMA PROGRAM : tabel_perkalian_menggunakan_while.pas  }
+
+program tabel_perkalian_menggunakan_while;
+uses crt;
+
+var
+i, n : integer ;
+
+begin
+clrscr;
+
+i := 0;
+write ('Masukan Angka Perkalian: '); readln(n);
+while i < n do
+begin
+writeln (n,' * ',i + 1, ' = ', (i + 1)*n);
+i := i + 1;
+end;
+
+readln;
+
+end.
